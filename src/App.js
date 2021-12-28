f5a05c1956e340e175ce276a9f885e8143c14245
Akash Dwivedi <91199423+akashdwivedi6261@users.noreply.github.com>
2021-12-28 16:52:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mobile Operating System</h1>
+      <ul className="align">
+        <li>
+          <div>Android</div>
+        </li>
+        <li>
+          <div>Blackberry</div>
+        </li>
+        <li>
+          <div>iPhone</div>
+        </li>
+        <li>
+          <div>Windows divhone</div>
+        </li>
+      </ul>
+      <h1>Mobile Manufactures</h1>
+      <ul className="align">
+        <li>
+          <div>Samsung</div>
+        </li>
+        <li>
+          <div>HTC</div>
+        </li>
+        <li>
+          <div>Micromax</div>
+        </li>
+        <li>
+          <div>Apple</div>
+        </li>
+      </ul>
+    </div>
+  );
+}
